cd785d7733e2a2e508ced4dc5581d3488057ac17
lexuanphi <tridatinh213@gmail.com>
2022-07-26 16:34:33 +0700

    phi.le test

--- src/main/java/com/example/controller/ControllerDemo.java
+++ src/main/java/com/example/controller/ControllerDemo.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class ControllerDemo {
+    Logger logger = Logger.getLogger(ControllerDemo.class.getName());
     @GetMapping(value = "/demo")
     public String Demo(){
+        logger.info("test log");
         return "xin chao tui la war";
     }
 }
